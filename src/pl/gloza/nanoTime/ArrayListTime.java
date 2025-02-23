0b3625a8c3e384e94fee675338239a5f45f5ae76
Aleksandra Gloza <aleksandra.gloza@gmail.com>
2025-02-23 07:54:15 +0100

    fix measuring time for 3 trials in a for loop, calculating average and refactor methods to return duration for the ArrayListTime and LinkedListTime class

--- src/pl/gloza/nanoTime/ArrayListTime.java
+++ src/pl/gloza/nanoTime/ArrayListTime.java
@@ -4,45 +4,115 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ArrayListTime {
-    public static List<Integer> fillArrayList(int size) {
+    public static long fillArrayList(int size) {
+        long startTime = System.nanoTime();
         List<Integer> arrayList = new ArrayList<>();
         for (int i = 0; i < size; i++) {
             arrayList.add(i + 1);
         }
-        return arrayList;
+        long endTime = System.nanoTime();
+        long duration = endTime - startTime;
+        System.out.println("Time of adding : " + duration + " ns");
+        return duration;
     }
 
-    public static List<Integer> removeEvenNumbers(List<Integer> numbers) {
-        for (int i = 0; i < numbers.size(); i++) {
-            if (numbers.get(i) % 2 == 0) {
-                numbers.remove(i);
-            }
-        }
-        return numbers;
+//    public static List<Integer> removeEvenNumbers(List<Integer> numbers) {
+//        for (int i = 0; i < numbers.size(); i++) {
+//            if (numbers.get(i) % 2 == 0) {
+//                numbers.remove(i);
+//            }
+//        }
+//        return numbers;
+//    }
+
+    public static long removeList(List<Integer> numbers) {
+        long startTime = System.nanoTime();
+        numbers.clear();
+        long endTime = System.nanoTime();
+        long duration = endTime - startTime;
+        System.out.println("Time of removing : " + duration + " ns");
+        return duration;
     }
 
     //zamień parzyste liczby na 0
-    public static List<Integer> updateValue(List<Integer> numbers) {
+    public static long updateValue(List<Integer> numbers) {
+        long startTime = System.nanoTime();
         for (int i = 0; i < numbers.size(); i++) {
             if (numbers.get(i) % 2 == 0) {
                 numbers.set(i, 0);
             }
         }
-        return numbers;
+        long endTime = System.nanoTime();
+        long duration = endTime - startTime;
+        System.out.println("Time of updating : " + duration + " ns");
+        return duration;
+    }
+
+    public static long readValues(List<Integer> numbers) {
+        long startTime = System.nanoTime();
+        for (int element : numbers) {
+            return element;
+        }
+        long endTime = System.nanoTime();
+        long duration = endTime - startTime;
+        System.out.println("Time of reading : " + duration + " ns");
+        return duration;
     }
 
     public static void main(String[] args) {
-        long startTime = System.nanoTime();
+        List<Integer> list = new ArrayList<>();
+        list.add(3);
+        list.add(1);
+        list.add(3);
+        list.add(2);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        list.add(6);
 
-        List<Integer> arrayList = fillArrayList(10000);
-        List<Integer> updatedList = updateValue(arrayList);
-        System.out.println(updatedList);
-//        List<Integer> evenNumbers = removeEvenNumbers(arrayList);
-//        System.out.println(evenNumbers);
+        for (int size = 100; size <= 10_000; size = size * 10) {
+            System.out.println("SIZE = " + size);
 
+            long[] fillResults = new long[3];
+            double sum = 0;
 
-        long endTime = System.nanoTime();
-        long duration = endTime - startTime;
-        System.out.println("Time: " + duration + " ns");
+            long[] removeResults = new long[3];
+            double sumRemoved = 0;
+
+            long[] updateResults = new long[3];
+            double sumUpdated = 0;
+
+            long[] readResults = new long[3];
+            double sumRead = 0;
+
+            for (int i = 0; i < 3; i++) {
+                fillResults[i] = fillArrayList(size);
+                sum += fillResults[i];
+
+                removeResults[i] = removeList(list);
+                sumRemoved += removeResults[i];
+
+                updateResults[i] = updateValue(list);
+                sumUpdated += updateResults[i];
+
+                readResults[i] = readValues(list);
+                sumRead += readResults[i];
+
+                System.out.println();
+            }
+
+            double average = sum / fillResults.length;
+            System.out.println("Average time of adding: " + average);
+
+            double average1 = sumRemoved / fillResults.length;
+            System.out.println("Average time of removing: " + average1);
+
+            double average2 = sumUpdated / fillResults.length;
+            System.out.println("Average time of updating: " + average2);
+
+            double average3 = sumRead / fillResults.length;
+            System.out.println("Average time of reading: " + average3);
+
+        }
     }
 }
--- src/pl/gloza/nanoTime/LinkedListTime.java
+++ src/pl/gloza/nanoTime/LinkedListTime.java
@@ -1,48 +1,121 @@
 package pl.gloza.nanoTime;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 public class LinkedListTime {
-    public static List<Integer> fillLinkedList(int size) {
-
+    public static long fillLinkedList(int size) {
+        long startTime = System.nanoTime();
         List<Integer> linkedList = new LinkedList<>();
         for (int i = 0; i < size; i++) {
             linkedList.add(i + 1);
         }
-        return linkedList;
+        long endTime = System.nanoTime();
+        long duration = endTime - startTime;
+        System.out.println("Time of adding : " + duration + " ns");
+        return duration;
     }
 
-    public static List<Integer> removeEvenNumbers(List<Integer> numbers) {
-        for (int i = 0; i < numbers.size(); i++) {
-            if (numbers.get(i) % 2 == 0) {
-                numbers.remove(i);
-            }
-        }
-        return numbers;
+//    public static List<Integer> removeEvenNumbers(List<Integer> numbers) {
+//        Iterator<Integer> iterator = numbers.iterator();
+//        while (iterator.hasNext()) {
+//            Integer element = iterator.next();
+//            iterator.remove();  // przechodzenie i usuwanie konkretnych elementów z listy
+//        }
+//        return numbers;
+//    }
+
+    public static long removeList(List<Integer> numbers) {
+        long startTime = System.nanoTime();
+        numbers.clear();
+        long endTime = System.nanoTime();
+        long duration = endTime - startTime;
+        System.out.println("Time of removing : " + duration + " ns");
+        return duration;
     }
 
     //zamień parzyste liczby na 0
-    public static List<Integer> updateValue(List<Integer> numbers) {
+    public static long updateValue(List<Integer> numbers) {
+        long startTime = System.nanoTime();
         for (int i = 0; i < numbers.size(); i++) {
             if (numbers.get(i) % 2 == 0) {
                 numbers.set(i, 0);
             }
         }
-        return numbers;
+        long endTime = System.nanoTime();
+        long duration = endTime - startTime;
+        System.out.println("Time of updating : " + duration + " ns");
+        return duration;
+    }
+
+    public static long readValues(List<Integer> numbers) {
+        long startTime = System.nanoTime();
+        for (int element : numbers) {
+            return element;
+        }
+        long endTime = System.nanoTime();
+        long duration = endTime - startTime;
+        System.out.println("Time of reading : " + duration + " ns");
+        return duration;
     }
 
     public static void main(String[] args) {
-        long startTime = System.nanoTime();
+        List<Integer> list = new ArrayList<>();
+        list.add(3);
+        list.add(1);
+        list.add(3);
+        list.add(2);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        list.add(6);
 
-        List<Integer> linkedList = fillLinkedList(10000);
-        List<Integer> updatedList = updateValue(linkedList);
-        System.out.println(updatedList);
-//        List<Integer> evenNumbers = removeEvenNumbers(linkedList);
-//        System.out.println(evenNumbers);
+        for (int size = 100; size <= 10_000; size = size * 10) {
+            System.out.println("SIZE = " + size);
 
-        long endTime = System.nanoTime();
-        long duration = endTime - startTime;
-        System.out.println("Time: " + duration + " ns");
+            long[] fillResults = new long[3];
+            double sum = 0;
+
+            long[] removeResults = new long[3];
+            double sumRemoved = 0;
+
+            long[] updateResults = new long[3];
+            double sumUpdated = 0;
+
+            long[] readResults = new long[3];
+            double sumRead = 0;
+
+            for (int i = 0; i < 3; i++) {
+                fillResults[i] = fillLinkedList(size);
+                sum += fillResults[i];
+
+                removeResults[i] = removeList(list);
+                sumRemoved += removeResults[i];
+
+                updateResults[i] = updateValue(list);
+                sumUpdated += updateResults[i];
+
+                readResults[i] = readValues(list);
+                sumRead += readResults[i];
+
+                System.out.println();
+            }
+
+            double average = sum / fillResults.length;
+            System.out.println("Average time of adding: " + average);
+
+            double average1 = sumRemoved / fillResults.length;
+            System.out.println("Average time of removing: " + average1);
+
+            double average2 = sumUpdated / fillResults.length;
+            System.out.println("Average time of updating: " + average2);
+
+            double average3 = sumRead / fillResults.length;
+            System.out.println("Average time of reading: " + average3);
+
+        }
+
+
     }
 }

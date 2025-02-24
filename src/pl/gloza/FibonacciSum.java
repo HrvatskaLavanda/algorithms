e9884c2f308913338de67f844004b55e72a9c9f0
Aleksandra Gloza <aleksandra.gloza@gmail.com>
2025-02-24 07:49:18 +0100

    optimize FibonacciSum

--- src/pl/gloza/FibonacciSum.java
+++ src/pl/gloza/FibonacciSum.java
@@ -3,7 +3,7 @@ package pl.gloza;
 import java.util.Scanner;
 
 public class FibonacciSum {
-    private static int fib(int n) {
+    private static int fibSum(int n) {
         if (n < 0) {
             System.out.println("Number n must be higher than 0");
             return -1;
@@ -14,29 +14,43 @@ public class FibonacciSum {
         if (n == 1) {
             return 1;
         } else {
-            return fib(n - 1) + fib(n - 2);
+//            return fib(n - 1) + fib(n - 2); //recursion
+            int f0 = 0;
+            int f1 = 1;
+            int sum = 0;
+
+            for (int i = 2; i <= n; i++) {
+                int f3 = f0 + f1;
+                f0 = f1;
+                f1 = f3;
+                sum += f1;
+            }
+            return sum;
         }
     }
 
-    private static int sumFibNums(int n) {
-        int sum = 0;
-        for (int i = 0; i <= n; i++) {
-            sum += fib(i);
-        }
-        return sum;
-    }
+    //for recursice version, change current above method fibSum into fib
+//    private static int sumFibNumsRecursive(int n) {
+//        int sum = 0;
+//        for (int i = 0; i <= n; i++) {
+//            sum += fib(i);
+//        }
+//        return sum;
+//    }
 
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Give a number: ");
         int n = scanner.nextInt();
-        int result = fib(n);
+
+        int result = fibSum(n);
         System.out.println("Fibonacci number: " + result);
 
-        int sumFibNums = sumFibNums(n);
-        System.out.println();
-        System.out.println("Sum: " + sumFibNums);
+        //recursive version:
+//        int sumFibNums = sumFibNumsRecursive(n);
+//        System.out.println();
+//        System.out.println("Sum: " + sumFibNums);
 
         scanner.close();
     }

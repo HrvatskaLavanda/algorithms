e5505042890afa7e64fc99623924a722303a1ed8
Aleksandra Gloza <aleksandra.gloza@gmail.com>
2025-02-28 09:52:18 +0100

    optimize method by using fori loop

--- src/pl/gloza/Palindrome.java
+++ src/pl/gloza/Palindrome.java
@@ -4,15 +4,22 @@ import java.util.Scanner;
 
 public class Palindrome {
     public static boolean isPalindrome(String word) {
-        int left = 0;
-        int right = word.length() - 1;
+//        int left = 0;
+//        int right = word.length() - 1;
+//
+//        while (left < right) {
+//            if (word.charAt(left) != (word.charAt(right))) {
+//                return false;
+//            }
+//            left++;
+//            right--;
+//        }
+//        return true;
 
-        while (left < right) {
-            if (word.charAt(left) != (word.charAt(right))) {
+        for (int i = 0; i < word.length() / 2; i++) {
+            if (word.charAt(i) != (word.charAt(word.length() - 1 - i))) {
                 return false;
             }
-            left++;
-            right--;
         }
         return true;
     }

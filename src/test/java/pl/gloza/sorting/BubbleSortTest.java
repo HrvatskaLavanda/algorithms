114a9e42e559c85552414b7274580562efacb252
Aleksandra Gloza <aleksandra.gloza@gmail.com>
2026-01-06 13:38:02 +0100

    (feat) add given-when-test to check if BubbleSort algorithm works correctly

--- src/test/java/pl/gloza/sorting/BubbleSortTest.java
+++ src/test/java/pl/gloza/sorting/BubbleSortTest.java
@@ -1,10 +1,27 @@
 package pl.gloza.sorting;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 class BubbleSortTest {
+    // 1. przetestowałabym czy rzeczywiście sortuje
+    // 2. wydajność dla długości tablicy 10, 100, 1000, 10_000 elementów
+    //3. czy tablica nie jest pusta
 
     @Test
-    void bubbleSort() {
+    void shouldBubbleSortCorrectly() {
+        //given
+        int[] nums = {1, 2, 5, 4, 10, 10, -1, 5};
+        int[] expected = nums.clone();
+        Arrays.sort(expected);
+
+        //when
+        int[] sortedNums = BubbleSort.bubbleSort(nums);
+
+        //then
+        Assertions.assertArrayEquals(expected, sortedNums);
+
     }
 }
